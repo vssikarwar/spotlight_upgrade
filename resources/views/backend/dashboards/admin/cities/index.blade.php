b34ad0c4e6c7e5a837698b43372f154346239ccb
vssikarwar <vishvendra.singh@singleinterface.com>
2022-07-04 15:20:45 +0530

    complete commit

--- resources/views/backend/dashboards/admin/cities/index.blade.php
+++ resources/views/backend/dashboards/admin/cities/index.blade.php
@@ -29,9 +29,8 @@
                     <th scope="col"><a href="/cities?direction=asc&amp;sort=id">Id</a></th>
                     <th scope="col" class="actions">Actions</th>
                     <th scope="col"><a href="/cities?direction=asc&amp;sort=name">Name</a></th>
-                    <th scope="col"><a href="/cities?direction=asc&amp;sort=iso_alpha_2">Iso Alpha 2</a></th>
+                    <th scope="col"><a href="/cities?direction=asc&amp;sort=iso_alpha_2">State</a></th>
                     
-                    <th scope="col"><a href="/cities?direction=asc&amp;sort=shopping_ads">Shopping Ads</a></th>
                     <th scope="col"><a href="/cities?direction=asc&amp;sort=active">Active</a></th>
                   
                   </tr>
@@ -44,15 +43,16 @@
 
                 <tr>
                         <td>{{ $city->id }}</td>
+                        <td class="actions">
+                        <a href="/cities/edit/1" class="btn btn-success btn-xs"><i class="fa fa-edit"></i></a>                    <form name="post_62a893c0decca776034106" style="display:none;" method="post" action="/cities/delete/1"><input type="hidden" name="_method" value="POST"/><input type="hidden" name="_csrfToken" autocomplete="off" value="97f3a760e55be43ddc3886e7f50501dba1bc5c04e2025d43bbafa6df3c159dae28472278b34a5573e20f6b7953fcbaabacbc07c68892993e8a57640f932e4349"/><div style="display:none;"><input type="hidden" name="_Token[fields]" autocomplete="off" value="693940990a9240a21c8ed5c2852b5ff0f6800209%3A"/><input type="hidden" name="_Token[unlocked]" autocomplete="off" value=""/></div></form><a href="#" class="btn btn-danger btn-xs" onclick="if (confirm(&quot;Are you sure you want to delete # 1?&quot;)) { document.post_62a893c0decca776034106.submit(); } event.returnValue = false; return false;"><i class="fa fa-trash"></i></a>                </td>
+               
                         <!-- <td><a href="/cities/view/104">India</a></td> -->
                         <td>Central Delhi</td>
                         <td>{{ $city->name }}</td>
                         
                         
                         <td><span class='label label-danger' style='color: red'>Inactive</span></td>
-                        <td class="actions">
-                        <a href="/cities/edit/1" class="btn btn-success btn-xs"><i class="fa fa-edit"></i></a>                    <form name="post_62a893c0decca776034106" style="display:none;" method="post" action="/cities/delete/1"><input type="hidden" name="_method" value="POST"/><input type="hidden" name="_csrfToken" autocomplete="off" value="97f3a760e55be43ddc3886e7f50501dba1bc5c04e2025d43bbafa6df3c159dae28472278b34a5573e20f6b7953fcbaabacbc07c68892993e8a57640f932e4349"/><div style="display:none;"><input type="hidden" name="_Token[fields]" autocomplete="off" value="693940990a9240a21c8ed5c2852b5ff0f6800209%3A"/><input type="hidden" name="_Token[unlocked]" autocomplete="off" value=""/></div></form><a href="#" class="btn btn-danger btn-xs" onclick="if (confirm(&quot;Are you sure you want to delete # 1?&quot;)) { document.post_62a893c0decca776034106.submit(); } event.returnValue = false; return false;"><i class="fa fa-trash"></i></a>                </td>
-                </tr>
+                         </tr>
 
                 @endforeach
          
